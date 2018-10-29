bef2c30df1183f56d420a50db3877ec79b4bd204
RenatoJavi <36644923+RenatoJavi@users.noreply.github.com>
2018-10-28 20:02:35 -0500

    uso de typeScript

---
+++ app.js
@@ -0,0 +1,8 @@
+//nombre tiene que ser un  string
+function saludar(nombre) {
+    console.log("Hola : " + nombre);
+}
+var lobitoObjeto = {
+    nombre: "jonas lindo perro"
+};
+saludar(lobitoObjeto.nombre);
---
+++ app.ts
@@ -0,0 +1,11 @@
+//nombre tiene que ser un  string
+function saludar(nombre) {
+    console.log("Hola : " + nombre);
+
+}
+
+var lobitoObjeto = {
+    nombre: "jonas lindo perro"
+};
+
+saludar(lobitoObjeto.nombre);
