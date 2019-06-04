2298e14459dede576f8db469f9c3be2c1168088d
RenatoJavi <rjavierse@hotmail.com>
2019-06-04 17:16:15 -0500

    regresar

--- Examen01/carrito/src/app/rutas/ruta-menu/ruta-menu.component.ts
+++ Examen01/carrito/src/app/rutas/ruta-menu/ruta-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ruta-menu',
@@ -7,21 +7,20 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./ruta-menu.component.css']
 })
 export class RutaMenuComponent implements OnInit {
-
   acciones = [
-    {titulo: 'Comprar',
-     path1: 'menu',
-     path2: 'comprar'},
-    {titulo: 'Administrar Grupos',
+    { titulo: 'Comprar', path1: 'menu', path2: 'comprar' },
+    {
+      titulo: 'Administrar Grupos',
       path1: 'menu',
-      path2: 'revisar-grupos'},
-    {titulo: 'Revisar Compras',
+      path2: 'revisar-grupos'
+    },
+    {
+      titulo: 'Revisar Compras',
       path1: 'menu',
-      path2: 'revisar-compra'}
+      path2: 'revisar-compra'
+    }
   ];
-  constructor(private readonly _activatedRoute: ActivatedRoute,
-              private readonly _router: Router
-              ) { }
+  constructor(private readonly _activatedRoute: ActivatedRoute, private readonly _router: Router) {}
 
   redirigir(path1, path2) {
     console.log('/menu', path2);
@@ -29,34 +28,27 @@ export class RutaMenuComponent implements OnInit {
     this._router.navigate(url);
   }
 
-
   ngOnInit() {
-    const parametros$ = this._activatedRoute.params;  //$ significa que es un observable
+    const parametros$ = this._activatedRoute.params;
     const parametrosConsulta$ = this._activatedRoute.queryParams;
-//Ambos parametros no necesitan las funciones "Catch" y "Complete" porque son especiallleeessss
 
-    parametrosConsulta$.subscribe(
-      (parametrosConsulta)=>{
-        console.log('Parametros consulta:', parametrosConsulta)
-      }
-    )
+    parametrosConsulta$.subscribe(parametrosConsulta => {
+      console.log('Parametros consulta:', parametrosConsulta);
+    });
 
     console.log('inicio');
-    parametros$
-    .subscribe(
-      (parametros) => {  //OK TRY
+    parametros$.subscribe(
+      parametros => {
         console.log('Parametros', parametros);
       },
-      (error) => {    //CATCH
+      error => {
         console.log('Error: ', error);
       },
-      () => { //Completado --> finally
+      () => {
         console.log('completo');
       }
-    )
+    );
 
     console.log('fin');
-
   }
-
 }
