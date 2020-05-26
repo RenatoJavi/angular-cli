f28a56b8023af54b733660f28f8af5a319c7bc29
RenatoJavi <rjavierse@hotmail.com>
2020-05-26 14:08:23 -0500

    configuracion del Nav bar

--- 12_spa/src/app/app.module.ts
+++ 12_spa/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { HeadComponent } from './componentes/head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule
