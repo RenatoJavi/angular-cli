6f0e28e68a372e83e71169e9a0292c5a3a315a24
NIco <industria_4.0@yahoo.com>
2020-07-21 10:48:02 -0500

    pathMatch
    En el menu.module.ts (agregar en la ruta por defecto pathMach:'full').
    En el menu.page.html ( se tiene la vista[ruta], que se configuro en menu.page.ts )

--- menu-navegacion/src/app/app-routing.module.ts
+++ menu-navegacion/src/app/app-routing.module.ts
@@ -2,33 +2,42 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
+
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'menu',
+    loadChildren: () =>
+      import('./pages/menu/menu.module').then((m) => m.MenuPageModule),
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'menu',
+    pathMatch: 'full',
   },
-  {
-    path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
-  },
+
   {
     path: 'menu-list',
-    loadChildren: () => import('./pages/menu-list/menu-list.module').then( m => m.MenuListPageModule)
+    loadChildren: () =>
+      import('./pages/menu-list/menu-list.module').then(
+        (m) => m.MenuListPageModule
+      ),
   },
   {
     path: 'contenido',
-    loadChildren: () => import('./pages/contenido/contenido.module').then( m => m.ContenidoPageModule)
+    loadChildren: () =>
+      import('./pages/contenido/contenido.module').then(
+        (m) => m.ContenidoPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
